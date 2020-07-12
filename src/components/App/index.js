fbd73363d41a951c06d295dd3bfdda83300e28c8
Emmanuel George <rustydcoder@gmail.com>
2020-07-12 01:58:47 +0100

    fixed AppJs className

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App.header">
+        <header className="App-header">
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Main />
